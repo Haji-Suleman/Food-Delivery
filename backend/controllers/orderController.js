ae38818b612a84647794eda7bb2b3b5b1af53f6d
user <you@example.com>
2025-05-20 20:35:35 +0500

    All is well

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -4,7 +4,7 @@ import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 //placing user order
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:5173";
+  const frontend_url = "https://food-delivery-azure-nine.vercel.app";
   try {
     const newOrder = new orderModel({
       userId: req.body.userId,
